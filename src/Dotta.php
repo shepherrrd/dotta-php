ddf34a5effcb2fb86a01f8df59419d511467ae06
shepherrrd <shefkid2@gmail.com>
2024-02-26 16:30:50 +0100

    Finished Face Detect

--- src/Dotta.php
+++ src/Dotta.php
@@ -3,6 +3,11 @@
 namespace Dotta;
 use Exception;
 use Dotta\Models\DottaResponse;
+use Dotta\Models\HttpDottaFaceAttributesResponse;
+use Dotta\Models\HttpDottaFaceAttributesResponseData;
+use Dotta\Models\HttpDottaFaceDetectResponse;
+use Dotta\Models\HttpDottaFaceDetectResponseData;
+use Dotta\Models\HttpDottaResponse;
 use Dotta\Enums\DottaEnvironment;
 
 class Dotta {
@@ -112,6 +117,84 @@ if ($response->getStatusCode() == 200){
         } catch (Exception $ex) {
             return new DottaResponse(false, $ex->getMessage());
         }
+}
+
+public function faceDetection($photo) {
+    $faceDetectResponse = new DottaResponse();
+
+    try {
+        // Check if photo file is empty
+        if ($photo === null) {
+            return new DottaResponse(false, "Photo with a face is required");
+        }
+
+        // Check for allowed photo file extension
+        $photoExtension = strtolower(pathinfo($photo['name'], PATHINFO_EXTENSION));
+        if (!in_array($photoExtension, $this->allowFileExtensions)) {
+            return new DottaResponse(false, "File extension not allowed. Allowed extensions are " . implode(" ", $this->allowFileExtensions));
+        }
+
+        // Get photo mime type
+        $photoMimeType = $this->getPhotoMimeType($photoExtension);
+        if (empty($photoMimeType)) {
+            return new DottaResponse(false, "Photo has an invalid mimetype");
+        }
+
+        // Build content for multipart HTTP request
+        $imageContent = fopen($photo['tmp_name'], 'r');
+        $multipart = [
+            [
+                'name'     => 'Photo',
+                'contents' => $imageContent,
+                'filename' => $photo['name'],
+                'headers'  => ['Content-Type' => $photoMimeType]
+            ]
+        ];
+
+        // Make network request and serialize response
+        $baseUrl = $this->environment == DottaEnvironment::PRODUCTION ? $this->baseUrlProduction : $this->baseUrlSandbox;
+        $response = $this->httpClient->request('POST', $baseUrl . '/Face/Detect', [
+            'headers' => ['Authorization' => 'Basic ' . base64_encode($this->apiKey)],
+            'multipart' => $multipart
+        ]);
+
+        $body = $response->getBody();
+        $data = json_decode($body, true);
+
+       if ($response->getStatusCode() == 200) {
+            // Success - Deserialize and map to FaceDetectResponse
+            $responseDTO = new HttpDottaFaceDetectResponse();
+            $responseDTO->status = $data['status'];
+            $responseDTO->message = $data['message'];
+
+            $responseDTO->data = new HttpDottaFaceDetectResponseData();
+            $responseDTO->data->angle = $data['data']['angle'] ?? null;
+            $responseDTO->data->errorCode = $data['data']['errorCode'] ?? null;
+            $responseDTO->data->errorMessage = $data['data']['errorMessage'] ?? null;
+            $responseDTO->data->orientation = $data['data']['orientation'] ?? null;
+            $responseDTO->data->padding = $data['data']['padding'] ?? null;
+            $responseDTO->data->pointX = $data['data']['pointX'] ?? null;
+            $responseDTO->data->pointY = $data['data']['pointY'] ?? null;
+            $responseDTO->data->width = $data['data']['width'] ?? null;
+            $faceDetectResponse->status = $responseDTO->status;
+            $faceDetectResponse->message = $responseDTO->message;
+            $faceDetectResponse->data = $responseDTO->data;
+        } else {
+            // Error - Deserialize and map to HttpDottaResponse
+            $errorResponse = new HttpDottaResponse();
+            $errorResponse->status = $data['status'];
+            $errorResponse->message = $data['message'];
+            $faceDetectResponse->status = $errorResponse->status;
+            $faceDetectResponse->message = $errorResponse->message;
+        }
+
+        fclose($imageContent);
+
+        return $faceDetectResponse;
+
+    } catch (Exception $ex) {
+        return new DottaResponse(false, $ex->getMessage());
+    }
 }
 
 private function getPhotoMimeType($photoExtension) {
